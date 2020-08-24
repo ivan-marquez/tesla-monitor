123c74a802a855bbcfc140df5b7b79e08c54d11c
Ivan Marquez <js.ivan.marquez@gmail.com>
2020-08-24 08:27:09 -0400

    NEW: adds local store package
    Add code to temporarily store data in case the database connection fails.

---
+++ pkg/localstore/store.go
@@ -0,0 +1,25 @@
+// Package localstore contains a single function to store a payload to a temporary store.
+// Use in case of network errors.
+package localstore
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+	"github.com/prologic/bitcask"
+)
+
+// SaveToLocalStore function to store payload to a temporary store
+func SaveToLocalStore(payload []byte) error {
+	db, err := bitcask.Open("/tmp/db")
+	if err != nil {
+		return fmt.Errorf("Error setting up local store: %v", err)
+	}
+
+	defer db.Close()
+
+	id := uuid.New()
+	db.Put(id.NodeID(), payload)
+
+	return nil
+}
